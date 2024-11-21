94d5e5ff9273928ea4c2956ec0104f19c8d46a84
hurjaffery <148715714+hurjaffery@users.noreply.github.com>
2024-11-21 10:50:18 +0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+// This is a simple Java program to print "Hello, World!" to the console.
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
